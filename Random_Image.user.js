a415d0403c3722521c75e45bc2b9e4f9da5e3faa
ccd0 <admin@containerchan.org>
2017-02-24 23:41:32 -0800

    First version.

---
+++ Random_Image.user.js
@@ -0,0 +1,122 @@
+// ==UserScript==
+// @name        Random Image
+// @namespace   https://github.com/ccd0
+// @include     http://boards.4chan.org/*
+// @include     https://boards.4chan.org/*
+// @version     0.0.1
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
+
+/* jshint esversion: 6 */
+
+let pick = (arr) =>
+  arr[Math.floor(Math.random() * arr.length)];
+
+let fetchJSON = (url) =>
+  new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.responseType = 'json';
+    req.onload = () => resolve(req.response);
+    req.onerror = req.onabort = reject;
+    req.send();
+  });
+
+let fetchImage = (url) =>
+  new Promise((resolve, reject) => {
+    GM_xmlhttpRequest({
+      method: 'GET',
+      url,
+      responseType: 'blob',
+      onload: (r) => resolve(r.response),
+      onerror: reject,
+      onabort: reject
+    });
+  });
+
+let pickBoard = () =>
+  fetchJSON('//a.4cdn.org/boards.json').then(response => {
+    let boards = response.boards.filter(x =>
+      x.ws_board &&
+      !x.text_only &&
+      x.board !== location.pathname.split(/\/+/)[1] &&
+      x.board !== 'mlp'
+    );
+    let board = pick(boards).board;
+    return Promise.resolve(board);
+  });
+
+let pickFile = (board) =>
+  fetchJSON(`//a.4cdn.org/${board}/catalog.json`).then(response => {
+    let replies = [];
+    response.forEach(p =>
+      p.threads.filter(t => t.last_replies).forEach(t =>
+        t.last_replies.filter(r => r.tim).forEach(r => {
+          replies.push(r);
+        })
+      )
+    );
+    let r = pick(replies);
+    let url = `http://i.4cdn.org/${board}/${r.tim}${r.ext}`;
+    let name = `${r.filename}${r.ext}`;
+    return Promise.resolve({url, name});
+  });
+
+let setFile = (file, name) => {
+  let detail = {file, name};
+  if (typeof cloneInto === 'function') {
+    detail = cloneInto(detail, document.defaultView);
+  }
+  let event = new CustomEvent('QRSetFile', {bubbles: true, detail});
+  document.dispatchEvent(event);
+  document.getElementById('qr').classList.add('dump');
+};
+
+let pickImage = () => {
+  let name;
+  pickBoard()
+    .then(pickFile)
+    .then(d => {
+      name = d.name;
+      return Promise.resolve(d.url);
+    }).then(fetchImage)
+    .then((file) => {
+      setFile(file, name);
+    });
+};
+
+let onQRExist = () =>
+  new Promise((resolve, reject) => {
+    let qr = document.getElementById('qr');
+    if (qr) {
+      resolve(qr);
+    } else {
+      document.addEventListener('QRDialogCreation', (e) => {
+        resolve(e.target);
+      }, false);
+    }
+  });
+
+let onQROpen = (cb) => {
+  onQRExist().then(qr => {
+    new MutationObserver(() => {
+      if (!qr.hidden) cb();
+    }).observe(qr, {
+      attributes: true,
+      attributeFilter: ['hidden']
+    });
+    if (!qr.hidden) cb();
+  });
+};
+
+let insertButton = () => {
+  let a = document.createElement('a');
+  a.className = 'qr-new-image fa fa-refresh';
+  a.style.opacity = '0.8';
+  a.addEventListener('click', pickImage, false);
+  let pos = document.getElementById('qr-spoiler-label');
+  pos.parentNode.insertBefore(a, pos.nextSibling);
+};
+
+onQRExist().then(insertButton);
+onQROpen(pickImage);
